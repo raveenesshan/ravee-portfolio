3cf687a795f217d46a7eeb3e4cea5d1e2b736078
Raveenesshan <raveenesshan@gmail.com>
2026-07-27 19:02:21 +0530

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
+
+  basePath: isGithubPages ? "/ravee-portfolio" : "",
+
+  assetPrefix: isGithubPages ? "/ravee-portfolio/" : "/",
 };
 
 export default nextConfig;
